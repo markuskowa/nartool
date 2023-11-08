c3aa890f732c35d4928450e84b36cb0d08bf6d9a
Markus Kowalewski <markus.kowalewski@gmail.com>
2023-11-08 21:53:06 +0100

    add flake

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs";
+
+  description = "NAR tool";
+
+  outputs = { self, nixpkgs }: {
+    packages.x86_64-linux.nartool = let
+      pkgs = import nixpkgs { system = "x86_64-linux"; };
+      inherit (pkgs.python3.pkgs)
+        buildPythonApplication
+        setuptools
+        requests;
+    in
+      buildPythonApplication {
+        pname = "nartool";
+        version = "0.0.1";
+
+        format = "pyproject";
+
+        src = ./.;
+
+        nativeBuildInputs =  [ setuptools ];
+        propagtedBuildInputs = [ requests ];
+      };
+  };
+}
